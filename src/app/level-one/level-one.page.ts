37a93b6c2ecb0dc830603b963a228eaf5fade433
krishna1048 <0217517.cst@rub.edu.bt>
2021-04-21 14:51:44 +0600

    Quiz questions adde

--- src/app/level-one/level-one.page.ts
+++ src/app/level-one/level-one.page.ts
@@ -17,10 +17,38 @@ export class LevelOnePage implements OnInit {
 
   ngOnInit() {
   }
+  ka() {
+    let sound = new Howl({
+      src: ['./assets/consonants/sound/ka.mp3']
+    });
+    sound.play();
+  }
 
+  ah() {
+    let sound = new Howl({
+      src: ['./assets/consonants/sound/ah.mp3']
+    });
+    sound.play();
+  }
   goToSlide() {
     // tslint:disable-next-line:triple-equals
     if (String(this.answer) == 'ཀ'){
+      console.log(String(this.answer));
+      // tslint:disable-next-line:no-unused-expression
+      this.score= this.score+1;
+      this.myService.successMusic();
+      console.log(String(this.score));
+      this.slides.slideTo(1, 500);
+    }
+
+    else{
+      console.log('False');
+      this.myService.failureMusic();
+      this.slides.slideTo(1, 500);
+    }
+  }
+  goToSlide1() {
+    if (String(this.answer) == 'ཨ'){
       console.log(String(this.answer));
       // tslint:disable-next-line:no-unused-expression
       this.score= this.score+1;
@@ -35,11 +63,27 @@ export class LevelOnePage implements OnInit {
       this.slides.slideTo(2, 500);
     }
   }
-
-  ka() {
+  nga() {
     let sound = new Howl({
-      src: ['./assets/consonants/sound/ka.mp3']
+      src: ['./assets/consonants/sound/nga.mp3']
     });
     sound.play();
+  }
+
+  goToSlide2() {
+    if (String(this.answer) == 'ང'){
+      console.log(String(this.answer));
+      // tslint:disable-next-line:no-unused-expression
+      this.score= this.score+1;
+      this.myService.successMusic();
+      console.log(String(this.score));
+      this.slides.slideTo(2, 500);
+    }
+
+    else{
+      console.log('False');
+      this.myService.failureMusic();
+      this.slides.slideTo(2, 500);
+    }
   }
 }
