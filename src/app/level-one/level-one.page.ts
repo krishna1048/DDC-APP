9a50149bca5ae8de6145e5ce2947f6c5599b6934
krishna1048 <0217517.cst@rub.edu.bt>
2021-05-16 22:28:58 +0600

    16 may

--- src/app/level-one/level-one.page.ts
+++ src/app/level-one/level-one.page.ts
@@ -3,6 +3,8 @@ import {SoundProviderService} from '../sound-provider.service';
 import {Howl, Howler} from 'howler';
 import { Router } from '@angular/router';
 import { ViewChild } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 import { IonSlides } from '@ionic/angular';
 @Component({
   selector: 'app-level-one',
@@ -14,7 +16,7 @@ export class LevelOnePage implements OnInit {
   answer: any = {};
   score: any = 0;
 
-  constructor(public myService: SoundProviderService) {
+  constructor(public myService: SoundProviderService, public alertController: AlertController) {
   }
 
   ngOnInit() {
@@ -48,15 +50,17 @@ export class LevelOnePage implements OnInit {
   }
 
   naro() {
-    let sound = new Howl({
-      src: ['./assets/yang-zhi/Naro.mp3']
+    var sound = new Howl({
+      src: ['./assets/Tenselo/yangzhi/naro.wav'],
+      volume:45
     });
     sound.play();
   }
 
   geku() {
-    let sound = new Howl({
-      src: ['./assets/Tenselo/yangzhi/geku.mp3']
+    var sound = new Howl({
+      src: ['./assets/Tenselo/yangzhi/giku.wav'],
+      volume:45
     });
     sound.play();
   }
@@ -66,9 +70,31 @@ export class LevelOnePage implements OnInit {
     });
     sound.play();
   }
-
+  btnTsa() {
+    var sound = new Howl({
+      src: ['./assets/consonants/sound/tsa.mp3']
+    });
+    sound.play();
+  }
+  btnCha() {
+    var sound = new Howl({
+      src: ['./assets/consonants/sound/cha.mp3']
+    });
+    sound.play();
+  }
+  btnTsha() {
+    var sound = new Howl({
+      src: ['./assets/consonants/sound/tsha.mp3']
+    });
+    sound.play();
+  }
+  btnZa() {
+    var sound = new Howl({
+      src: ['./assets/consonants/sound/za.mp3']
+    });
+    sound.play();
+  }
   goToSlide() {
-    // tslint:disable-next-line:triple-equals
     if (String(this.answer) == 'ཀ') {
       console.log(String(this.answer));
       // tslint:disable-next-line:no-unused-expression
@@ -76,6 +102,9 @@ export class LevelOnePage implements OnInit {
       this.myService.successMusic();
       console.log(String(this.score));
       this.slides.slideTo(1, 500);
+
+
+      // this.slides.slideTo(1, 500);
     } else {
       console.log('False');
       this.myService.failureMusic();
@@ -159,6 +188,80 @@ export class LevelOnePage implements OnInit {
       console.log('False');
       this.myService.failureMusic();
       this.slides.slideTo(6, 500);
+    }
+  }
+
+  goToSlide6() {
+    if (String(this.answer) == 'ས') {
+      console.log(String(this.answer));
+      // tslint:disable-next-line:no-unused-expression
+      this.score = this.score + 1;
+      this.myService.successMusic();
+      console.log(String(this.score));
+      this.slides.slideTo(7, 500);
+    }
+    else {
+      console.log('False');
+      this.myService.failureMusic();
+      this.slides.slideTo(7, 500);
+    }
+  }
+  goToSlide7() {
+    if (String(this.answer) == 'ཆ') {
+      console.log(String(this.answer));
+      // tslint:disable-next-line:no-unused-expression
+      this.score = this.score + 1;
+      this.myService.successMusic();
+      console.log(String(this.score));
+      this.slides.slideTo(8, 500);
+    }
+    else {
+      console.log('False');
+      this.myService.failureMusic();
+      this.slides.slideTo(8, 500);
+    }
+  }
+  goToSlide8() {
+    if (String(this.answer) == 'ཚ') {
+      console.log(String(this.answer));
+      // tslint:disable-next-line:no-unused-expression
+      this.score = this.score + 1;
+      this.myService.successMusic();
+      console.log(String(this.score));
+      this.slides.slideTo(9, 500);
+    }
+    else {
+      console.log('False');
+      this.myService.failureMusic();
+      this.slides.slideTo(9, 500);
+    }
+  }
+  async goToSlide9() {
+    if (String(this.answer) == 'ཟ') {
+      console.log(String(this.answer));
+      // tslint:disable-next-line:no-unused-expression
+      this.score = this.score + 1;
+      this.myService.successMusic();
+      console.log(String(this.score));
+      if (this.score>=0){
+        const alert = await this.alertController.create({
+          cssClass: 'my-custom-class',
+          header: '<img src="./assets/birds/image/blacked_n_crane.jpg">\n',
+          subHeader: 'Subtitle',
+          message: 'This is an alert message.',
+          buttons: ['OK', 'Next']
+        });
+
+        await alert.present();
+
+        const { role } = await alert.onDidDismiss();
+        console.log('onDidDismiss resolved with role', role);
+      }
+    }
+    else {
+      console.log('False');
+      this.myService.failureMusic();
+      this.slides.slideTo(10, 500);
     }
   }
 }
